d6e7d87a5f9d70ec855c9eca2775ca4d8aaa06df
nannnnna <141238876+nannnnna@users.noreply.github.com>
2023-09-28 00:05:04 +0300

    urls.py

--- urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('buy/<int:item_id>/', views.get_session_id, name='get_session_id'),
-    path('item/<int:item_id>/', views.item_detail, name='item_detail'),
-]
